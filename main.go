6154ee0dab5ce12a4bd04927502c4d9faf90ccb7
tomaer <i@tomaer.com>
2024-01-19 15:32:31 +0800

    初版提交

--- main.go
+++ main.go
@@ -1,50 +1,37 @@
 package main
 
 import (
-	"fmt"
-	"io/ioutil"
-	"path/filepath"
+    "fmt"
+	"time"
+    "io/ioutil"
+    "os"
+	"strings"
 )
 
-// 定义当前目录信息
-type currentDirInfo struct {
-	currentDirs  []string
-	currentFiles []string
+func getCurrentDir(path string) {
+    infos, err := ioutil.ReadDir(path)
+	if err!= nil {
+        fmt.Println(err)
+    }
+    for _, info := range infos {
+		if (info.IsDir()) {
+			fmt.Printf("当前是目录, 相对路径是: %s/%s\n", path,info.Name())
+			getCurrentDir(path + "/" + info.Name())
+		} else {
+			if (strings.Contains(info.Name(), "-tr_TR_") || strings.Contains(info.Name(), "-sv_SE_") || strings.Contains(info.Name(), "-sl_SI_") || strings.Contains(info.Name(), "-ru_RU_") || strings.Contains(info.Name(), "-ro_RO_") || strings.Contains(info.Name(), "-pt_PT_") || strings.Contains(info.Name(), "-pt_BR_") || strings.Contains(info.Name(), "-pl_PL_") || strings.Contains(info.Name(), "-nl_NL_") || strings.Contains(info.Name(), "-ja_JP_") || strings.Contains(info.Name(), "-ko_KR_") || strings.Contains(info.Name(), "-it_IT_") || strings.Contains(info.Name(), "-hu_HU_") || strings.Contains(info.Name(), "-hr_HR_") || strings.Contains(info.Name(), "-fr_FR_") || strings.Contains(info.Name(), "-fi_FI_") || strings.Contains(info.Name(), "-es_ES_") || strings.Contains(info.Name(), "-en_GB_") || strings.Contains(info.Name(), "-el_GR_") || strings.Contains(info.Name(), "-da_DK_") || strings.Contains(info.Name(), "-cs_CZ_")) {
+				fmt.Printf("正在删除需要精简的语言包文件, 相对路径是: %s/%s\n", path, info.Name())
+				err := os.Remove(path+"/"+info.Name())
+				if err!= nil {
+                    fmt.Printf("文件删除失败，当前文件的相对路径为: %s/%s\n", path, info.Name())
+                }
+			}
+		}
+	}
 }
-
-func getCurrentDir(path string) (currentDirInfo, error) {
-	infos, err := ioutil.ReadDir(path)
-	if err != nil {
-	   return currentDirInfo{}, nil
-	}
-	var currentDirs []string
-	var currentFiles []string
-	for _, info := range infos {
-	   fmt.Printf("Name:%-30s    Size:%-10d字节    Modtime:%s    IsDir:%v\n", info.Name(), info.Size(), info.ModTime().Format("2006-01-02 03:04:05"), info.IsDir())
-	   abs, err := filepath.Abs(info.Name())
-	   if err != nil {
-		  return currentDirInfo{}, nil
-	   }
-	   if info.IsDir() {
-		  currentDirs = append(currentDirs, abs)
-	   } else {
-		  currentFiles = append(currentFiles, abs)
-	   }
-	}
-	fmt.Printf("=======================当前文件夹==============================\n")
-	for _, v := range currentDirs {
-	   fmt.Printf("当前文件夹:%s\n", v)
-	}
-	fmt.Printf("========================当前文件=============================\n")
-	for _, v := range currentFiles {
-	   fmt.Printf("当前文件:%s\n", v)
-	}
- 
-	return currentDirInfo{currentDirs: currentDirs, currentFiles: currentFiles}, nil
- }
- func main() {
-	_, err := getCurrentDir(".")
-	if err != nil {
-	   return
-	}
- }
+func main() {
+	fmt.Printf("欢迎使用ODIS/ODIS-E Postsetup语言包精简工具,程序将会在10秒后自动开始\n")
+	time.Sleep(10 * time.Second)
+    getCurrentDir(".")
+	fmt.Printf("程序已经精简完成，可以检查一下Postsetup数据包的体积啦\n")
+	time.Sleep(20 * time.Second)
+}
